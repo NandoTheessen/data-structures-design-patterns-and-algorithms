4775b69036edc4a0fa244f28921d420f986ab5d0
NandoTheessen <nando.theessen@gmail.com>
2018-07-30 14:19:53 +0200

    Initial commit

---
+++ mergesort.py
@@ -0,0 +1,28 @@
+#! usr/bin/env
+"""Simple script to present a possible way of coding mergesort"""
+import sys
+
+
+def mergesort_helper(array):
+    """Uses the mergesort algorythm to sort
+    the given array
+
+    Args:
+        unsorted array
+    returns:
+        sorted Array"""
+    def mergesort(left, right):
+        pass
+    pass
+
+
+def main(array):
+    """executes all functions in this script
+
+    Args:
+        Array given by the user
+    """
+    mergesort_helper(array)
+
+if __name__ == '__main__':
+    main(sys.argv[1])
